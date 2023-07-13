c87b54c5f9bc7bd40dfa7412b1f1e5ef50634b60
jo <jo@kali>
2023-07-13 12:40:00 -0400

    first

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - good
+ *Return: (0)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float e;
+	printf("Size of a char: %ld byte(s)\n",sizeof(a));
+	printf("Size of an int: %ld byte(s)\n",sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n",sizeof(c));
+	printf("Size of a long long: %ld byte(s)\n",sizeof(d));
+	printf("Size of a float: %ld byte(s)\n",sizeof(e));
+
+	return (0);
+}
