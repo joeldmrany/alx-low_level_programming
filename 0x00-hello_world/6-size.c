f8cf84e648dc64e9ab6765a2eaeb24bef4487ebf
jo <jo@kali>
2023-07-13 13:54:05 -0400

    sixth

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long int c;
 long long d;
 float e;
 
-printf("Size of a char: %zu byte(s)\n", sizeof(a));
-printf("Size of an int: %zu byte(s)\n", sizeof(b));
-printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-printf("Size of a long long: %zu byte(s)\n", sizeof(d));
-printf("Size of a float: %zu byte(s)\n", sizeof(e));
+printf("Size of a char: %ld byte(s)\n", sizeof(a));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of a float: %ld byte(s)\n", sizeof(e));
 return (0);
 }
