738b25f92d830917b1ab9a68edef43dafc4654d0
jo <jo@kali>
2023-07-14 11:56:36 -0400

    first

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - fine
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
