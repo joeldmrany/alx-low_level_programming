8c09249d067b2b22895888f372525307c2b44672
jo <jo@kali>
2023-07-14 12:50:07 -0400

    first

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - fine
+ * Return: (0)
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
