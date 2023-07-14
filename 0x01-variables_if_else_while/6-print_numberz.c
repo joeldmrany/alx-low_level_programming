d9a3a3891401ee1c1d76646f7b02d322cea29106
jo <jo@kali>
2023-07-14 12:26:03 -0400

    first

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - comment
+ * Return: (0)
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
