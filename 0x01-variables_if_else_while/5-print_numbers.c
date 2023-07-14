79775f6f330afd5ab0370b1df005ea3e6ab08474
jo <jo@kali>
2023-07-14 12:20:17 -0400

    first

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - good
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
