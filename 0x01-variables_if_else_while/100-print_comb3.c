fa30636cc65437e03e9826eff32ba7da552e2a6c
jo <jo@kali>
2023-07-17 13:01:37 -0400

    first

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - tmam
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = (a + 1); b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
