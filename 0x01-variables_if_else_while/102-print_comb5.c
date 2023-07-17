c0f51dd18f24b90ca94ac2f03cbead5a7f48ac09
jo <jo@kali>
2023-07-17 14:29:09 -0400

    first

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - good
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = (b + 1); d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+
+					if (!((a == '9' && b == '8')&&(c == '9' && d == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
