40cd9af1ae4030cdef36ff3c3203e8b60f8205cb
jo <jo@kali>
2023-07-19 12:48:28 -0400

    first

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - good
+ * Return: (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = '0'; b <= '10'; b++)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
