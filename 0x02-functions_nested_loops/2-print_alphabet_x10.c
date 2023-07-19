9641f9162b5c9180b1ad2c143d17c9e0bdbf578d
jo <jo@kali>
2023-07-19 12:58:06 -0400

    third

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,14 @@ void print_alphabet_x10(void)
 {
 	char a;
 	int b;
-	
-	a = 'a';
 
 	for (b = '0'; b < '10'; b++)
 	{
-		while (a <= 'z')
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
-			a++;
 		}
 		_putchar('\n');
+	
 	}
 }
