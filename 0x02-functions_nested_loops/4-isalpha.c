6545599a502bfa3f8e2d74cbfc8cf7dfe0ec65a0
jo <jo@kali>
2023-07-19 16:22:54 -0400

    first

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - not as it is
+ * @c: fine today
+ * Return: (0)
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
