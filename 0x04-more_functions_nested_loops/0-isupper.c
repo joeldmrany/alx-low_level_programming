7fed9d6540e7070148fdb98bacc9d425b064a5cf
joeldmrany <joeldmrany1@gmail.com>
2023-07-21 08:50:37 -0400

    first

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - main
+ * @c: is upper case
+ * Return: 1 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
