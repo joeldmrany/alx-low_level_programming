bad9bb85611ca2a23d9c54cccd328c72dafdbb30
joeldmrany <joeldmrany1@gmail.com>
2023-07-26 19:33:33 -0400

    final

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - like printf
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
