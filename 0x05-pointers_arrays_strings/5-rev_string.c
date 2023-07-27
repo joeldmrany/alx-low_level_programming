d9c970b9d2ce74fd6638c3aac7279be6e3a489a0
joeldmrany <joeldmrany1@gmail.com>
2023-07-27 08:20:17 -0400

    seventh

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int a, b, c;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	b = a - 1;
+	c = b;
+	while (c >= 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+
+	_putchar('\n');
+}
