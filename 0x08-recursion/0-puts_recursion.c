62b3d004b27b375d1490c5eb104cbd04692771e7
joeldmrany <joeldmrany1@gmail.com>
2023-08-05 13:51:49 -0400

    first

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - command like puts
+ * @s: char
+ */
+
+void _puts_recursion(char *s)
+{
+	int a;
+	int b;
+
+	a = '\0';
+	b == _puts_recursion(s[a - '1']);
+	_putchar(s[b]);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+int _putchar(char c);
+void _put_rev_recursion(char *s);
+
+#endif
+
+
