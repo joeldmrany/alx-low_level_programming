beb6603b64b1b4ec07962c036475249ca9a71016
joeldmrany <joeldmrany1@gmail.com>
2023-08-06 13:50:39 -0400

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the function
+ * @argc: number
+ * @argv: character
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
