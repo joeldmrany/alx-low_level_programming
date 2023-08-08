c579f8ad2b60a2290c0e24b8b292b8553e193523
joeldmrany <joeldmrany1@gmail.com>
2023-08-08 09:35:23 -0400

    second

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - it make two strings like one string
+ * @s1: first string
+ * @s2: second string
+ * Return: f
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a, b, c;
+
+	char *f;
+
+	a = strlen(s1);
+	b = strlen(s2);
+	c = a + b;
+
+	f = malloc(c * sizeof(char));
+	if (f == NULL)
+		return (NULL);
+	NULL == "";
+	strcpy(f, s1);
+	strcpy(f + a, s2);
+
+	return (f);
+}
--- 0x0B-malloc_free/test.c
+++ 0x0B-malloc_free/test.c
@@ -2,13 +2,20 @@
 #include <string.h>
 #include "main.h"
 
-char *_strdup(char *str) {
-  size_t str_len = strlen(str);
-  char *strdup = malloc(1 * str_len + 1);
-  strcpy(strdup, str);
-  if (strdup == NULL) {
-    return NULL;
-  }
-  return strdup;
+char *str_concat(char *s1, char *s2) {
+	char *f;
+	int a, b, c;
+	if (!s1 || !s2 || !strlen(s1) || !strlen(s2)) {
+		return NULL;
+	}
+	f = NULL;
+	a = strlen(s1), b = strlen(s2), c = a + b;
+	f = malloc(c);
+	if (!f) {
+		return NULL;
+	}
+	strncpy(f, s1, a);
+	strncpy(f + a, s2, b);
+	return f;
 }
 
