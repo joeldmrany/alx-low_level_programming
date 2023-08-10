27270d68c3dc3d4c9c0cdef3e0ad817fe25f96c1
joeldmrany <joeldmrany1@gmail.com>
2023-08-10 19:27:29 +0300

    third

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* string_nconcat(char* s1, char* s2, unsigned int n) {
+	char* string_nconcat(char* s1, char* s2, unsigned int n) {
     char* d = malloc(strlen(s1) + strlen(s2) + 1);
 
     memcpy(d, s1, strlen(s1));
@@ -12,6 +12,20 @@ char* string_nconcat(char* s1, char* s2, unsigned int n) {
         memcpy(d + strlen(s1), s2, n);
     } else {
         memcpy(d + strlen(s1), s2, strlen(s2));
+    }
+
+    if (d == NULL) {
+        free(s1);
+        free(s2);
+        return NULL;
+    }
+
+    memcpy(d, s1, strlen(s1));
+
+    if (n >= strlen(s2)) {
+        memcpy(d + strlen(s1), s2, strlen(s2));
+    } else {
+        memcpy(d + strlen(s1), s2, n);
     }
 
     return d;
