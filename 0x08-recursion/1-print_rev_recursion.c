0e4c9ca35f9dbc9d1909d771297d6ea9e67b62d1
joeldmrany <joeldmrany1@gmail.com>
2023-08-11 21:03:29 +0300

    first

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ * _print_rev_recursion - prints int reverse
+ * @s: it is the string that will be added
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
