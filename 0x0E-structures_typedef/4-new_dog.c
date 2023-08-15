16d48d5a457c8b4907169740477ae314247326fb
joeldmrany <joeldmrany1@gmail.com>
2023-08-15 23:27:12 +0300

    first

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,10 +46,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
+	}
 	dog->name = _strcpy(dog->name, name);
 	dog->owner = _strcpy(dog->owner, owner);
 	dog->age = age;
