0d404467a4422ec4d59e072393d57740140dc1f9
joeldmrany <joeldmrany1@gmail.com>
2023-08-15 14:33:37 +0300

    first

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H_
 #define DOG_H_
-
+/**
+ * struct dog - structure to change the type
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ */
 struct dog
 {
 	char *name;
