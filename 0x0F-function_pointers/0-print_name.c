66df5e381d93c0818a8d94ea60366b74907bd392
joeldmrany <joeldmrany1@gmail.com>
2023-08-16 15:42:33 +0300

    first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - prints the name
  * @name: the name of the person
- * f - the funtion that will be called
+ * @f: the funtion that will be called
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
