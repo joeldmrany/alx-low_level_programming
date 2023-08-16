ba0e51f058ce3e78ba4334fa945202d36b97c8e6
joeldmrany <joeldmrany1@gmail.com>
2023-08-16 16:05:19 +0300

    first

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * int_index - function
+ * @array: the array
+ * @size: the size
+ * @cmp: the compare function
+ * Return: integer
+ */
+int int_index(int *array, int size, int (cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+}
