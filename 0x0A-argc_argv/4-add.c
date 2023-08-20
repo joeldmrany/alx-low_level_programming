74f3acf5fe8363337627d6952c25e12186727a91
joeldmrany <joeldmrany1@gmail.com>
2023-08-20 07:44:02 +0300

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
 
+/**
+ * main - Program the add all arguments
+ * @argc: Number of arguments
+ * @argv: argument
+ * Return: 1 or 0
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int a, b;
+	int i, a, length, sum;
+	char *ptr;
 
-	if (argc >= 1)
-	{
-		for (a = 1; a <= argc; a++)
-		{
-			if (atoi(argv[a]) <= '9' && atoi(argv[a]) >= '0')
-			{
-				b += atoi(argv[a]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", b);
-	}
-	else if (argc <= 1)
-	{
+	if (argc < 2)
 		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (a = 0; a < length; a++)
+			{
+				if (isdigit(*(ptr + a)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
 	}
 	return (0);
 }
