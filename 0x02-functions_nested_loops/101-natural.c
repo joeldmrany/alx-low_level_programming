4a0f92360a8c9fe264646e39565df60cf5c515ef
joeldmrany <joeldmrany1@gmail.com>
2023-08-23 20:36:43 +0300

    giving the sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_sum_of_them - prints the sum of numbers multiples by 3 or 5
+ */
 
 void print_sum_of_them(void)
 {
@@ -11,10 +13,18 @@ void print_sum_of_them(void)
 		if ((a % 3) == 0)
 		{
 			sum += a;
+			if ((a % 5) == 0)
+			{
+				sum += a;
+			}
 		}
 		else if ((a % 5) == 0)
 		{
 			sum += a;
+			if ((a % 3) == 0)
+			{
+				sum += a;
+			}
 		}
 	}
 	printf("%d\n", sum);
