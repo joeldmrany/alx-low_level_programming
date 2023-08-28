ff1f3103053a35b8b9a83225f37c51be6b18ddba
joeldmrany <joeldmrany1@gmail.com>
2023-08-28 17:14:56 +0300

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints the string in reverse
+ * @s: the string pointer
+ */
+void print_rev(char *s)
+{
+	int a, b;
+
+	for (a = 0; s[a]; a++)
+	{
+	}
+	for (b = a; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
