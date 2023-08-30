85d1d220edca20fcdcec6a793c1ec5848a2f039a
joeldmrany <joeldmrany1@gmail.com>
2023-08-30 15:25:33 +0300

    4 task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recuresion - return the result of x power y
+ * @x: the number
+ * @y: the power
+ * Return: the result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return x;
+}
