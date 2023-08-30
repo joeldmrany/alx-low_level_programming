662bf7c395c03314ded236470cf3648f5da4b480
joeldmrany <joeldmrany1@gmail.com>
2023-08-30 14:27:22 +0300

    0 task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints the string in recursion way
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
