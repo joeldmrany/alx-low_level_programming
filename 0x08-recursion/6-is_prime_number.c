3ca53b2eef2ed1624e1ce200db18ffc344481727
joeldmrany <joeldmrany1@gmail.com>
2023-08-30 16:16:56 +0300

    6 task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - check if n is prime or not
+ * @n: the number
+ * Return: 1 if prime 0 if not
+ */
+int checker(int n, int c);
+int is_prime_number(int n)
+{
+	return (checker(n, 2));
+}
+/**
+ * checker - check the prime number
+ * @n: the number
+ * @c: the checker
+ * Return: 1 or 0
+ */
+int checker(int n, int c)
+{
+	if (n == c && n > 1)
+		return (1);
+	else if (n % c == 0 || n <= 1)
+		return (0);
+	else
+		return (checker(n, c + 1));
+}
