264b0f771aafd98ff8a21600bed030c962d9ed36
joeldmrany <joeldmrany1@gmail.com>
2023-09-01 18:39:07 +0300

    1 task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
