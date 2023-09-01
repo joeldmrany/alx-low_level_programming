19e8b9c992221751b02f85306b58c7c7e3fd34e8
joeldmrany <joeldmrany1@gmail.com>
2023-09-01 18:35:20 +0300

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - that is the main task
+ * @argc: the number of arguments
+ * @argv: the argument array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
