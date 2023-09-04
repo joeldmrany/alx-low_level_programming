2127b202c9824f746df48c866540ac9733fc9189
joeldmrany <joeldmrany1@gmail.com>
2023-09-04 12:42:52 +0300

    1 task: attempt 0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - free space to string
+ * @str: the string
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	int a, b;
+	char *string;
+
+	a = strlen(str);
+	string = malloc(a * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+	{
+		string[b] = str[b];
+	}
+	return (string);
+}
