a8050d2f8ef6fe205efe26495444651cbf1497c8
joeldmrany <joeldmrany1@gmail.com>
2023-09-04 12:32:39 +0300

    0 task: attempt 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 	arr = malloc(size * sizeof(char));
 	if (arr == 0)
 		return (NULL);
+	if (size == 0)
+		return (NULL);
 	for (a = 0; a <= size; a++)
 	{
 		arr[a] = c;
