29abbb2e14854fd0fe83c29134fc3fdf02098ae8
joeldmrany <joeldmrany1@gmail.com>
2023-09-07 07:40:38 +0300

    task: 0, attempt: 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * malloc_checked - do like malloc
+ * @b: the space reserved
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
