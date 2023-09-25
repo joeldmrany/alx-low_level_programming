6b9aea0880a960cb46ab6fb2b9679b28712a05f4
joeldmrany <joeldmrany1@gmail.com>
2023-09-25 14:56:40 +0300

    task: 1, attempt:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,4 @@
-#ifndef DOG
-#define DOG
-/**
- * struct dog - define
- * @name: the name of the dog
- * @age: the age
- * @owner: teh owner
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-#endif
+#ifndef DOG_H_
+#define DOG_H_
+#define struct dog {
+	char *name
