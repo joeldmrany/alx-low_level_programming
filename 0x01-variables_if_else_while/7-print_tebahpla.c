1df60f7e35684dd61714ede758cf56e99a70137c
joeldmrany <joeldmrany1@gmail.com>
2024-03-03 19:30:10 +0200

    task: 7, attempt: 0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * i: counter
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
