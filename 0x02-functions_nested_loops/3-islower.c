78b4f730ee30d45492ce7b62996ca33e9e374f8d
joeldmrany <joeldmrany1@gmail.com>
2024-03-04 10:08:42 +0200

    task: 3, attempt: 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks if the letter is lower case of not
+ * Return: 1 if it's lower 0 otherwise
+ */
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
