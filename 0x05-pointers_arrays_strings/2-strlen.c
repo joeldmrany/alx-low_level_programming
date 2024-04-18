c336b240f72afb57df37775867612cd1b6d8b5a3
joeldmrany <joeldmrany1@gmail.com>
2024-04-18 15:27:31 +0200

    task: 2, attempt: 0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of the string
+ * @s: string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
