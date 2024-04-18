6865d57f7c3833946868ca71173af706e7ac55f0
joeldmrany <joeldmrany1@gmail.com>
2024-04-18 15:46:05 +0200

    task: 9, attempt: 0

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copying
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
