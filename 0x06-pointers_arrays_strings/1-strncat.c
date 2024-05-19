fc0e69077c5c0eb343ecc5d15748f5815d9d0b16
joeldmrany <joeldmrany1@gmail.com>
2024-05-19 15:50:51 +0300

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strncat: function near to strcat
+ * @dest: destination
+ * @src: source
+ * @n: number of bits
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b, c;
+	char *d;
+
+	a = strlen(dest);
+	b = strlen(src);
+	c = a + b;
+	if (n >= c)
+	{
+		d = malloc((c + 1) * sizeof(char));
+		strcpy(d, dest);
+		strcpy(d + a, src);
+		d[c] = '\0';
+		strcpy(dest, d);
+		return (d);
+	}
+	else
+	{
+		d = malloc((c + 1) * sizeof(char));
+		strcpy(d, dest);
+		strncpy(d + a, src, n);
+		d[a + n] = '\0';
+		strcpy(dest, d);
+		return (d);
+	}
+}
