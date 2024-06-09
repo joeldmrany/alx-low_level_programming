d26924c2faf3cbd18c79952cc3dddbe797b28d92
joeldmrany <joeldmrany1@gmail.com>
2024-06-09 17:21:27 +0300

    task: 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
@@ -16,5 +16,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
+	head = NULL;
 }
