28a62536e7824792c596c158234aacd6eaee6b5b
joeldmrany <joeldmrany1@gmail.com>
2024-06-09 17:10:57 +0300

    task: 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees lists
+ * @head: pointer to list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
