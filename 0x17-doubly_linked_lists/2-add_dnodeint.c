ecbcd5b0e9c2eebda951d8861fad520810e23b17
joeldmrany <joeldmrany1@gmail.com>
2024-06-09 21:35:28 +0300

    task: 2g

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,13 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	if (head == NULL)
-		return (NULL);
-	new_node->prev = NULL;
 	new_node->n = n;
-	new_node->next = *head;
-	if (head != NULL)
+	new_node->prev = NULL;
+	if (*head != NULL)
 		(*head)->prev = new_node;
+	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
